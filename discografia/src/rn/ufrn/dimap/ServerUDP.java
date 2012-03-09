a09af225584134a30287fcbb99689e1ebbaba742
olivb@ig.com.br <olivb@ig.com.br@a853e0ac-00ae-8b66-f1aa-5a6682c92272>
2012-03-09 16:27:07 +0000

    Servidor básico para teste dos componentes

---
+++ discografia/src/rn/ufrn/dimap/ServerUDP.java
@@ -0,0 +1,35 @@
+package rn.ufrn.dimap;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class ServerUDP {
+	public static void main(String args[]) throws SocketException {
+        DatagramSocket s = null;
+        
+        s = new DatagramSocket(6789);
+        System.out.println("Servidor: aguardando pedidos");
+        
+        while(true){
+        
+	        try {
+	            
+	            byte[] buffer = new byte[1000];
+	            DatagramPacket req = new DatagramPacket(buffer, buffer.length);
+	            s.receive(req);
+	            System.out.println("Servidor: recebeu: " + req.getAddress()+":"+req.getPort() + " " + new String(req.getData()));
+	            
+	            // envia resposta
+	            DatagramPacket resp = new DatagramPacket(req.getData(), req.getLength(),
+	                    req.getAddress(), req.getPort());
+	            s.send(resp);
+	        } catch (SocketException e) {
+	            System.out.println("Erro de socket: " + e.getMessage());
+	        } catch (IOException e) {
+	            System.out.println("Erro envio/recepcao pacote: " + e.getMessage());
+	        } finally {}
+        }
+    }
+}
