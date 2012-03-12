71b0b422705e1a515df827b852570691e0a9741d
olivb@ig.com.br <olivb@ig.com.br@a853e0ac-00ae-8b66-f1aa-5a6682c92272>
2012-03-12 21:38:37 +0000

    Especialização da classe UDPMessage usando Thread para enviar mensagens

---
+++ discografia/src/rn/ufrn/dimap/UDPMessageThreadSend.java
@@ -0,0 +1,17 @@
+package rn.ufrn.dimap;
+
+import java.net.DatagramSocket;
+
+public class UDPMessageThreadSend extends UDPMessage implements Runnable {
+
+	public UDPMessageThreadSend(DatagramSocket socket, String sender) {
+		super(socket);
+		super.setMessage(sender);
+	}
+
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
