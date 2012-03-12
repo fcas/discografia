6e8b5f792777996ffff17edcf1b10cdd0a28c372
olivb@ig.com.br <olivb@ig.com.br@a853e0ac-00ae-8b66-f1aa-5a6682c92272>
2012-03-12 21:25:29 +0000

    Classe que armazena a estrutura basica da discografia

---
+++ discografia/src/rn/ufrn/dimap/Disco.java
@@ -0,0 +1,39 @@
+package rn.ufrn.dimap;
+
+public class Disco {
+	private int ano;
+	private String album;
+	private String tipo;
+	
+	public Disco(int ano, String album, String tipo) {
+		super();
+		this.ano = ano;
+		this.album = album;
+		this.tipo = tipo;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+
+	public String getAlbum() {
+		return album;
+	}
+
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+}
