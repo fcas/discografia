46180115ba9438bcebdde9d98752679ede5c1cd4
olivb@ig.com.br <olivb@ig.com.br@a853e0ac-00ae-8b66-f1aa-5a6682c92272>
2012-03-13 22:55:01 +0000

    Classe usada para testar conectividade entre alguns componentes da aplicacao

---
+++ trunk/discografia/src/rn/ufrn/dimap/Connection.java
@@ -0,0 +1,69 @@
+package rn.ufrm.dimap;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+
+public class Connection {
+
+	private String separator = null;
+	private String fileName = null;
+	private InputStream fileIn=null;
+	private Scanner in=null;
+	private String ip=null;
+	private int port;
+	private DatagramSocket socket=null;
+	private DatagramPacket sender=null;
+	private DatagramPacket receiver=null;
+	private String agent;
+	private InetAddress addr;
+	
+	
+	
+	public Connection(String fileName, String agent) {
+		this.fileName = fileName;
+		this.agent=agent;
+		
+		loadFile();
+		
+		
+	}
+	
+	private void loadFile(){
+		
+		try {
+			this.fileIn = new FileInputStream(fileName);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	
+	public  InetAddress getConnection(){
+		
+		if (this.fileIn !=null){
+			
+			
+			
+			while(in.hasNext()){
+				
+			}
+			
+			
+		}
+		
+		return null;
+	}
+	
+	
+	
+	
+	
+}
