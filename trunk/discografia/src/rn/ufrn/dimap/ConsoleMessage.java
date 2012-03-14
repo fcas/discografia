be88c1b370625c5ea0bd82e19856696bd69e66d6
olivb@ig.com.br <olivb@ig.com.br@a853e0ac-00ae-8b66-f1aa-5a6682c92272>
2012-03-14 14:12:10 +0000

    Criando os comentarios da classe ConsoleMessage

--- trunk/discografia/src/rn/ufrn/dimap/ConsoleMessage.java
+++ trunk/discografia/src/rn/ufrn/dimap/ConsoleMessage.java
@@ -1,36 +1,73 @@
 package rn.ufrn.dimap;
 
 
+/**
+ * Classe usada para customizar
+ * as menssagens dos componente 
+ * e messagens de erro
+ *
+ */
 public class ConsoleMessage {
 	private String agent;
 	private String message;
 	
+	
+	/**
+	 * Constroi o objeto para
+	 * @param agent o nome do componte da aplicao que esta usando a classe
+	 * @param messagem a mensagem que o componte que criar
+	 */
 	public ConsoleMessage(String agent, String messagem) {
 		super();
 		setAgent(agent);
 		setMessagem(messagem);
 	}
 
+	/**
+	 * Obetem o componte que esta usando a classe
+	 * @return o nome do componente
+	 */
 	public String getAgent() {
 		return agent;
 	}
 
+	
+	/**
+	 * Configura o nome do componente
+	 * @param agent o nome do componente
+	 */
 	private void setAgent(String agent) {
 		this.agent = agent;
 	}
 
+	
+	/**
+	 * Obtem a messagem 
+	 * @return a messagem
+	 */
 	public String getMessagem() {
 		return message;
 	}
 
+	/**
+	 * Configura o mensagem do componente
+	 * @param messagem
+	 */
 	private void setMessagem(String messagem) {
 		this.message = messagem;
 	}
 	
+	
+	/**
+	 * Cria uma saida padrao customizada
+	 */
 	public void defaultOut(){
 		System.out.printf("%s:%s.\n",this.agent,this.message);
 	}
 	
+	/**
+	 * Cria uma saida de erro customizada
+	 */
 	public void outErro(){
 		System.err.printf("%s:%s.\n",this.agent,this.message);
 	}
