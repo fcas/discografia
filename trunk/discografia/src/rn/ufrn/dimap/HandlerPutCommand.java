6ccbdee79cf81824c70b3c9d76dcbd7736fc983f
olivb@ig.com.br <olivb@ig.com.br@a853e0ac-00ae-8b66-f1aa-5a6682c92272>
2012-03-16 13:51:35 +0000

    Atualizando a lista de artistas

--- trunk/discografia/src/rn/ufrn/dimap/HandlerPutCommand.java
+++ trunk/discografia/src/rn/ufrn/dimap/HandlerPutCommand.java
@@ -1,17 +1,28 @@
 package rn.ufrn.dimap;
 
 /**
- * Trantando o comando Put
+ * Trantando o comando get
  */
-
 public class HandlerPutCommand extends Handler {
 
+	private MapArtist mapArtist = null;
+	private SystemConfigurations sysConfig = null;
+	
 	@Override
 	public void handleRequest(Request request) {
 		
-		/* Se for o comando put proceder a resposta */
+		this.mapArtist = new MapArtist();
+		this.sysConfig = new SystemConfigurations();
+		
 		if (request.getCommand().equals(Commands.PUT)){
 			
+			String del = sysConfig.getDELIMITED_FIELD();
+			String artista = request.getArgument();
+			String info = request.getInfo();
+			
+			// adicionado a informacao 
+			mapArtist.add(String.format("%s", artista.split(del)[0]),info);
+				
 		}else{
 			
 			if (successor != null){
@@ -21,4 +32,14 @@ public class HandlerPutCommand extends Handler {
 		}
 	}
 
+	/**
+	 * Retorna a lista de artista 
+	 * @return the mapArtist
+	 */
+	public MapArtist getMapArtist() {
+		return mapArtist;
+	}
+
+	
+	
 }
