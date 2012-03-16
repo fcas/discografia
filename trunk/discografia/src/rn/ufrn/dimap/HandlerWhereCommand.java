c6a3a7350e5cbe148fbc464eb70222e8cca53757
olivb@ig.com.br <olivb@ig.com.br@a853e0ac-00ae-8b66-f1aa-5a6682c92272>
2012-03-16 16:49:45 +0000

    Ok

--- trunk/discografia/src/rn/ufrn/dimap/HandlerWhereCommand.java
+++ trunk/discografia/src/rn/ufrn/dimap/HandlerWhereCommand.java
@@ -5,12 +5,15 @@ package rn.ufrn.dimap;
  * 
  */
 public class HandlerWhereCommand extends Handler {
-
+	private ConsoleMessage consoleMsg = null;
+	
 	@Override
 	public void handleRequest(Request request) {
 		
 		/* Se for o comando put proceder a resposta */
 		if (request.getCommand().equals(Commands.WHERE)){
+			consoleMsg.setMessagem("O comando WHERE sera tratado.");
+			consoleMsg.print();
 			
 		}else{
 			
