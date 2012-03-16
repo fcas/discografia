84385dd0f75ee3fb812e46163d1c0fff436953a1
olivb@ig.com.br <olivb@ig.com.br@a853e0ac-00ae-8b66-f1aa-5a6682c92272>
2012-03-16 16:57:53 +0000

    Ok

--- discografia/src/rn/ufrn/dimap/ClientUDPTest.java
+++ discografia/src/rn/ufrn/dimap/ClientUDPTest.java
@@ -3,9 +3,6 @@ package rn.ufrn.dimap;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.SocketException;
-import java.net.UnknownHostException;
-import java.util.Scanner;
 
 public class ClientUDPTest {
 	public static void main(String[] args) {
