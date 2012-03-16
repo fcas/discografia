874113dd76601ceb20da032fcc1135bf7e639ef3
olivb@ig.com.br <olivb@ig.com.br@a853e0ac-00ae-8b66-f1aa-5a6682c92272>
2012-03-16 13:50:54 +0000

    Adicionando o comando SYNC

--- trunk/discografia/src/rn/ufrn/dimap/Commands.java
+++ trunk/discografia/src/rn/ufrn/dimap/Commands.java
@@ -1,14 +1,23 @@
 package rn.ufrn.dimap;
 
-/**
- * Enumerador usado para tratar os comandos dos protocolo da aplicacao
- */
 
+/**
+ * Enumerador usado para tratar os omandos dos protocolo da aplicacao:
+ * 
+ * echo - usado para teste de conexao
+ * data - usado para enviar dados 
+ * get  - solicitar dados de discografias
+ * put  - enviar dados de artistas
+ * sync - solicitar dados de syncrolizacao de dados
+ * disc - envia dados de discografias
+ *  
+ */
 public enum Commands {
 	ECHO,
 	DATA,
 	GET,
 	PUT,
 	WHERE,
+	SYNC,
 	DISCO
 }
