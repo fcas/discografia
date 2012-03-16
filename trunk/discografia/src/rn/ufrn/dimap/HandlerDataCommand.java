150fef9094f25eca203bf4008874cafa8bd426b1
olivb@ig.com.br <olivb@ig.com.br@a853e0ac-00ae-8b66-f1aa-5a6682c92272>
2012-03-16 16:47:24 +0000

    Ok

--- trunk/discografia/src/rn/ufrn/dimap/HandlerDataCommand.java
+++ trunk/discografia/src/rn/ufrn/dimap/HandlerDataCommand.java
@@ -5,12 +5,16 @@ package rn.ufrn.dimap;
  */
 public class HandlerDataCommand extends Handler {
 
+	private ConsoleMessage consoleMsg = null;
+	
 	@Override
 	public void handleRequest(Request request) {
+		consoleMsg = new ConsoleMessage();
 		
 		// se for o comando get proceder a resposta
 		if (request.getCommand().equals(Commands.DATA)){
-			//
+			consoleMsg.setMessagem("O comando DATA sera tratado.");
+			consoleMsg.print();
 		}else{
 			
 			if (successor != null){
