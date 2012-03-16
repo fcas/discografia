a08add1c423bac4f7d5f4cbf3e8f32be2351277b
olivb@ig.com.br <olivb@ig.com.br@a853e0ac-00ae-8b66-f1aa-5a6682c92272>
2012-03-16 16:47:07 +0000

    Teste a classe

--- discografia/src/rn/ufrn/dimap/DataUDP.java
+++ discografia/src/rn/ufrn/dimap/DataUDP.java
@@ -13,11 +13,14 @@ public final class DataUDP {
 	private UDPSendMessage sendMessage = null;
 	private SystemConfigurations sysConfig = null;
 	private Scanner scan = null;
+	private HandlerCommand handlerCommand = null; 
+	private ConsoleMessage consoleMessage = null;
 	
 	public DataUDP (int port){
 		this.port = port;
 		this.scan = new Scanner(System.in);
 		this.sysConfig = new SystemConfigurations();
+		this.consoleMessage = new ConsoleMessage();
 	}
 	
 	public void listenKB(){
@@ -29,20 +32,35 @@ public final class DataUDP {
 		}
 	}
 	
-	public void lineCatch(String linha){
-			
+	public void lineCatch(String linha){ 
+		consoleMessage.setMessagem("Comando mal formado.");
+		
 		/* Procura o delimitador das mensagens */
 		if (linha.contains(sysConfig.getDELIMITED_FIELD())){
 				
 			/* Obtendo os campos comando e argumento */
 			String cmd = linha.split(sysConfig.getDELIMITED_FIELD())[0];
 			String arg = linha.split(sysConfig.getDELIMITED_FIELD())[1];
-			HandlerCommand (cmd, arg);
+			
+			try {
+				handlerCommand = new HandlerCommand(cmd, arg);
+			} catch (Exception e) {
+				consoleMessage.print();
+			}
+			
 		} else {
-			System.out.printf("Comando mal formado: %s",linha);
+			consoleMessage.print();
 	      }
 	}
 	
+	
+	/**
+	 * @param handlerCommand the handlerCommand to set
+	 */
+	public void setHandlerCommand(HandlerCommand handlerCommand) {
+		this.handlerCommand = handlerCommand;
+	}
+
 	public static void main(String[] args) {
 		
 		try {
