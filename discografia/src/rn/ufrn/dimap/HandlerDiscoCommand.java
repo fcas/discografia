545d84200d3c50fd3d9dc9184f7386723a306bbd
olivb@ig.com.br <olivb@ig.com.br@a853e0ac-00ae-8b66-f1aa-5a6682c92272>
2012-03-16 16:47:51 +0000

    Ok

--- discografia/src/rn/ufrn/dimap/HandlerDiscoCommand.java
+++ discografia/src/rn/ufrn/dimap/HandlerDiscoCommand.java
@@ -5,12 +5,16 @@ package rn.ufrn.dimap;
  */
 public class HandlerDiscoCommand extends Handler {
 
+	private ConsoleMessage consoleMsg = null;
+	
 	@Override
 	public void handleRequest(Request request) {
+		consoleMsg = new ConsoleMessage();
 		
 		// se for o comando get proceder a resposta
 		if (request.getCommand().equals(Commands.DISCO)){
-			//
+			consoleMsg.setMessagem("O comando DISCO sera tratado.");
+			consoleMsg.print();
 		}else{
 			
 			if (successor != null){
