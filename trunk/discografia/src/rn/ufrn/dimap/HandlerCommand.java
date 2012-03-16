e4d88675d21c2285a2c28ac9b5fe28cb5bd1161e
olivb@ig.com.br <olivb@ig.com.br@a853e0ac-00ae-8b66-f1aa-5a6682c92272>
2012-03-16 16:56:30 +0000

    ok

--- trunk/discografia/src/rn/ufrn/dimap/HandlerCommand.java
+++ trunk/discografia/src/rn/ufrn/dimap/HandlerCommand.java
@@ -1,32 +1,130 @@
 package rn.ufrn.dimap;
 
-public void class HandlerCommand (){
-	
-	private Request request = null;
-	
-	public void handlercommand (String cmd, String arg) {
+public class HandlerCommand{
 
-	Commands enumCommand;
-		
-	Handler handlerGet = new HandlerGetCommand();
-	Handler handlerData = new HandlerDataCommand();
-	Handler handlerDisco = new HandlerDiscoCommand();
-	Handler handlerEcho = new HandlerEchoCommand();
-	Handler handlerPut = new HandlerPutCommand();
+	private Handler handlerGet = null;
+	private Handler handlerPut = null;
+	private Handler handlerData = null;
+	private Handler handlerDisco = null;
+	private Handler handlerEcho = null;
+	private Handler handlerSync = null;
+	private Request request = null;
+	private Commands enumCommand;
+	private String command;
+	private String argument;
 	
-	handlerGet.setSucessor(handlerData);
-	handlerData.setSucessor(handlerDisco);
-	handlerDisco.setSucessor(handlerEcho);
-	handlerEcho.setSucessor(handlerPut);
+	/**
+	 * Constroi o objeto tatrador de comandos
+	 * @param cmd o comando a tratado
+	 * @param arg o argumento do comando
+	 * @throws repassa o Exception para o componente
+	 */
+	public HandlerCommand (String cmd, String arg) throws Exception {
+
+		// criando as instancias do handler
+		createInstances();
 	
-	try {
-		
+		// verificando se o comando e o esperado
 		enumCommand = Commands.valueOf(cmd);
-		request = new Request(enumCommand, arg);
-		handlerGet.handleRequest(request);
 		
-	} catch (Exception e) {
-		System.out.printf("Comando nao implementado: %s",cmd);
+		// criando os dados da requisicao
+		request = new Request(enumCommand, arg);
+		
+		chainOfResponsibility(); 				// defina a sucessao
+		handlerGet.handleRequest(request); 		// inicia a cadeia
+		
 	}
-  }
+	
+		
+	/**
+	 * Obtem o comando solicitado
+	 * @return the command
+	 */
+	public String getCommand() {
+		return command;
+	}
+
+	/**
+	 * @param command the command to set
+	 */
+	public void setCommand(String command) {
+		this.command = command;
+	}
+
+	/**
+	 * @return the argument
+	 */
+	public String getArgument() {
+		return argument;
+	}
+
+	/**
+	 * @param argument the argument to set
+	 */
+	public void setArgument(String argument) {
+		this.argument = argument;
+	}
+
+	/**
+	 * Cria a instancias dos handles dos comandos 
+	 * 
+	 */
+	private void createInstances(){
+		
+		handlerGet = new HandlerGetCommand();
+		handlerPut = new HandlerPutCommand();
+		handlerData = new HandlerDataCommand();
+		handlerDisco = new HandlerDiscoCommand();
+		handlerEcho = new HandlerEchoCommand();
+		handlerSync = new HandlerSyncCommand();
+		
+	}
+	
+	
+	public void setArgs(String cmd, String arg){
+		this.command=cmd;
+		this.argument=arg;
+	}
+	
+	/**
+	 * Iniciando a cadeia de responsabilidade
+	 */
+	private void chainOfResponsibility(){	
+		
+		handlerGet.setSucessor(handlerPut);
+		handlerPut.setSucessor(handlerData);
+		handlerData.setSucessor(handlerDisco);
+		handlerDisco.setSucessor(handlerEcho);
+		handlerEcho.setSucessor(handlerSync);
+		
+	}
+
+	/**
+	 * @return the handlerSync
+	 */
+	public Handler getHandlerSync() {
+		return handlerSync;
+	}
+
+	/**
+	 * @return the handlerPut
+	 */
+	public Handler getHandlerPut() {
+		return handlerPut;
+	}
+
+	/**
+	 * @return the request
+	 */
+	public Request getRequest() {
+		return request;
+	}
+
+	/**
+	 * @param request the request to set
+	 */
+	public void setRequest(Request request) {
+		this.request = request;
+	}
+	
 }
