13a3a8a40e9c354b1c3c69fd3b25c8ff93b694f8
olivb@ig.com.br <olivb@ig.com.br@a853e0ac-00ae-8b66-f1aa-5a6682c92272>
2012-03-19 10:22:34 +0000

    Classe finalizada

--- trunk/discografia/src/rn/ufrn/dimap/UDPReceiveMessage.java
+++ trunk/discografia/src/rn/ufrn/dimap/UDPReceiveMessage.java
@@ -6,106 +6,64 @@ import java.net.DatagramSocket;
 import java.net.SocketException;
 
 
-/**
- * Classe para receber mensagens UDP
- *
- */
 public class UDPReceiveMessage {
 	
+	private UDPMessageContent messageContent = null;
 	private DatagramSocket socket = null;
 	private DatagramPacket packet = null;
-	private String menssage = null;
-	private SystemConfigurations sysConfig = null;
-	private int port;
+	private SystemConfigurations sys = null;
+	private byte[] buffer = null;
+	private String text = null;
 	
-	
-	/**
-	 * Construtor padrao
-	 */
-	public UDPReceiveMessage() {
-		try {
-			socket = new DatagramSocket();
-		} catch (SocketException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	/**
-	 * Constroi o objeto de para receber uma mensagem UDP
-	 * @param port a porta de recebimento do pacote
-	 * @throws SocketException repassando o erro para componente 
-	 */
-	public UDPReceiveMessage(int port) throws SocketException {
-		this.sysConfig = new SystemConfigurations(); 
-		this.port = port;
+	public UDPReceiveMessage() throws SocketException {
+		
+		sys = new SystemConfigurations();
+		
 	}
 	
 	
-	/** Configura um DatagramSocket
+	public void receive() throws IOException{
+		
+		int length = sys.getMAX_LENGTH_MESSAGE();
+		// preparando um objeto para receber
+		// o conteudo cifrado
+		messageContent = new UDPMessageContent(length);
+		buffer = new byte[length];
+		packet = new DatagramPacket(buffer,buffer.length);
+		socket.receive(packet);
+		// passando o array de bytes recebido 
+		// decrifar
+		messageContent.setBytes(packet.getData());
+		setText(messageContent.getText());
+		
+	}
+	
+	public synchronized String getTex(){
+		return text;
+	}
+	
+	public synchronized void setText(final String text){
+		this.text = text;
+	}
+	
+	public synchronized DatagramPacket getPacket() {
+		return packet;
+	}
+
+
+	/**
 	 * @param socket the socket to set
 	 */
 	public void setSocket(DatagramSocket socket) {
 		this.socket = socket;
 	}
-	
-	/**
-	 * recebe a mensagem UDP
-	 * @throws IOException
-	 */
-	public void receive() throws IOException{
-		
-		byte[] arrayByte = new byte[sysConfig.getMAX_LENGTH_MESSAGE()];
-		packet = new DatagramPacket(arrayByte,arrayByte.length);
-		socket.receive(packet);
-		
-		String data;
-		data = new String(packet.getData());
-		
-		setMessage(data);
-
-	}
-	
-
-	/**
-	 * Obtem a porta para receber a mensagem
-	 * @return port o porta da envio da mensagem
-	 */
-	public int getPort() {
-		return port;
-	}
 
 
 	/**
-	 * Configura a porta para recebimento
-	 * da mensagem
-	 * @param port a porta para recebimento
+	 * @return the socket
 	 */
-	public void setPort(int port) {
-		this.port = port;
-	}
-
-	public String getMessage(){
-		return menssage;
+	public DatagramSocket getSocket() {
+		return socket;
 	}
 	
-	private void setMessage(String text){
-		this.menssage=text;
-	}
-	
-	/* teste simple de envio de mensagem
-	public static void main(String[] args) {
-		try {
-			
-			UDPReceiveMessage mensagem = new UDPReceiveMessage(1025);
-			
-			new UDPSendMessage("ola", InetAddress.getByName("localhost"),1025).sender();
-			mensagem.receive();
-			System.out.println(mensagem.getMessage());
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-	}
-	*/
 }
