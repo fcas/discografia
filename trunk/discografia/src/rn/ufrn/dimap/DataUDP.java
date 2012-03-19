2d4a48d070f5d8f279ac61235ffb75b66bfae342
olivb@ig.com.br <olivb@ig.com.br@a853e0ac-00ae-8b66-f1aa-5a6682c92272>
2012-03-19 22:14:16 +0000

    Falta implementar as respostas

--- trunk/discografia/src/rn/ufrn/dimap/DataUDP.java
+++ trunk/discografia/src/rn/ufrn/dimap/DataUDP.java
@@ -1,10 +1,9 @@
 package rn.ufrn.dimap;
 
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
 import java.net.SocketException;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public final class DataUDP {
 
@@ -12,127 +11,219 @@ public final class DataUDP {
 	private Request request = null;
 	private UDPReceiveMessage receiveMessage = null;
 	private UDPSendMessage sendMessage = null;
-	private SystemConfigurations sysConfig = null;
-	private Scanner scan = null;
 	private HandlerCommand handlerCommand = null;
-	private ConsoleMessage consoleMessage = null;
-	private String artist;
+	private Connection connection = null;
+	private SystemConfigurations sys = null;
+	private List<String>listArtist = null;
+	private String agent = null;
 
-	public DataUDP(String artis, int port) {
-		
-		this.artist = artis;
+	/**
+	 * Constroi o componente DataProvider 
+	 * @param port
+	 * @throws SocketException
+	 */
+	public DataUDP(int port,String agent, List<String>artists) {
+
+		this.listArtist = artists;
 		this.port = port;
-		this.scan = new Scanner(System.in);
-		this.sysConfig = new SystemConfigurations();
-		this.consoleMessage = new ConsoleMessage();
-		consoleMessage.setAgent("Dataprovider");
-		consoleMessage.setMessagem("Iniciando");
-		
-		// enviando os dados
-		try {
-			handlerCommand = new HandlerCommand("PUT",this.artist);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
+		this.agent = agent;
+		createInstances();
+		 
 	}
 
+	/**
+	 * Coloca o dataProvider 
+	 * para receber as mensagens
+	 */
 	public void listenIt() {
-		
-		String contentMessage;
 
-		/*
-		 * consoleMessage.setMessagem("Informa artista"); artist =
-		 * scan.nextLine();
-		 * 
-		 * while(true) {
-		 * 
-		 * System.out.println("PUT - Enviar discografia\n"); String userMessage
-		 * = scan.nextLine(); lineCatch(userMessage); }
-		 */
-		DatagramSocket s=null;
-		
+		String message = null;
+		System.out.printf("%s: iniciando na porta %s\n",agent,port);
+
 		try {
-			s = new DatagramSocket(1090);
-		} catch (SocketException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+			checkServers();
+		} catch (IOException e) {
+			System.err.printf("%s: %createInstancess%s%s\n",agent,"[",e.getMessage(),"]");
 		}
 		
 		while (true) {
 
-			byte[]b = new byte[1024];
-			
-			
-			//receiveMessage = new UDPReceiveMessage();
-			//receiveMessage.setSocket(socket);
-			DatagramPacket p;
-			
+
 			try {
 
+				receiveMessage = new UDPReceiveMessage();
+				receiveMessage.setSocket(connection.getSocket());
 				
-				p = new DatagramPacket(b, b.length);
-				s.receive(p);
-				
-				//receiveMessage.receive();
-				contentMessage =  new String(p.getData());
-				//contentMessage = receiveMessage.getMessage();
-				
-				lineCatch(contentMessage);
+				receiveMessage.receive();
+				String hostD = receiveMessage.getPacket().getAddress().getHostName();
+				int portD = receiveMessage.getPacket().getPort();
+				message = receiveMessage.getTex();
+
+				if (message != null) {
+
+					lineCatch(message,String.format(":%s:%s", hostD, portD));
+
+				}
 
 			} catch (IOException e) {
-				e.printStackTrace();
-			} catch (Exception e) {
-				e.printStackTrace();
+
+				System.err.printf("%s: %s\n",agent,e.getMessage());
+
 			}
 
 		}
 
 	}
 
-	public void lineCatch(String linha) {
-		consoleMessage.setMessagem("Comando mal formado.");
+	public void lineCatch(String message, String extra) {
 
-		/* Procura o delimitador das mensagens */
-		if (linha.contains(sysConfig.getDELIMITED_FIELD())) {
+		String del = sys.getDELIMITED_FIELD();
 
-			/* Obtendo os campos comando e argumento */
-			String cmd = linha.split(sysConfig.getDELIMITED_FIELD())[0];
-			String arg = linha.split(sysConfig.getDELIMITED_FIELD())[1];
+		if (message.contains(sys.getDELIMITED_FIELD())) {
+
+			String cmd = message.split(del)[0];
+			String arg = message.split(del)[1];
+			String type = message.split(del)[2];
+			String host = extra.split(del)[0];
+			int port = Integer.parseInt(extra.split(del)[1]);
 
 			try {
-				handlerCommand = new HandlerCommand(cmd, arg);
+
+				handlerCommand = new HandlerCommand(cmd, arg, type, host, port);
 
 			} catch (Exception e) {
-				consoleMessage.print();
+
+				System.err.printf("%s: %s\n",agent,e.getMessage());
+
 			}
 
 		} else {
-			consoleMessage.setMessagem("Delimitador não encontrado");
-			consoleMessage.print();
+			System.out.printf("%s: comando mal formado %s\n",agent,message);
 		}
+	}
+
+	private void checkServers() throws IOException{
+		
+		String del = sys.getDELIMITED_FIELD();
+		String server = null;
+				
+		System.out.printf("%s: procurando um servidor disponivel\n",agent);
+		server = connection.testServers();
+		
+		if (server !=null){
+			
+			String hostD = server.split(del)[0];
+			int portD = Integer.parseInt(server.split(del)[1]);
+			System.out.printf("%s: o servidor %s:%s esta disponivel\n",agent,hostD,portD);
+			
+			for (String artis : listArtist) {
+				
+				System.out.println(String.format("%s: enviando discografia '%s' para %s:%s",agent,artis,hostD,portD));
+				String menssage = String.format("%s:%s:%s","PUT",artis,"D");
+				sendData(menssage, hostD, portD);
+				
+			}
+			
+		}else{
+			
+			System.err.printf("%s: nenhum servidor esta disponivel.",agent);
+			
+		}
+			
+	}	
+	
+	/**
+	 * Envia ma mensagem para 
+	 * @param content
+	 * @param host
+	 * @param port
+	 */
+	private void sendData(String content,String host, int port){
+		
+		try {
+			
+			sendMessage = new UDPSendMessage(content.getBytes());
+			sendMessage.setSocket(connection.getSocket()); 
+			sendMessage.setPort(port);
+			sendMessage.setInetAdress(host);
+			sendMessage.send();
+			
+		} catch (Exception e) {
+			System.out.printf("%s - erro ao enviar %s para %s:%s\n",agent,content,host,port);
+		}
+		
+		
+	}
+	
+	
+	/**
+	 * @return the request
+	 */
+	public Request getRequest() {
+		return request;
+	}
+	
+	
+	/**
+	 * Cria as instancia do objentos necessarios
+	 * no componente
+	 * @throws SocketException
+	 */
+	private void createInstances(){
+		
+		try {
+			
+			this.sys = new SystemConfigurations();
+			this.connection = new Connection(port); 
+			listArtist = new ArrayList<String>();  
+			
+		} catch (IOException e) {
+			System.err.printf("%s",e.getMessage());
+		}
+		
+		
+	}
+	
+	/**
+	 * @return the handlerCommand
+	 */
+	public HandlerCommand getHandlerCommand() {
+		return handlerCommand;
+	}
+
+	
+	/**
+	 * @return the agent
+	 */
+	public String getAgent() {
+		return agent;
 	}
 
 	/**
-	 * @param handlerCommand
-	 *            the handlerCommand to set
+	 * @param agent the agent to set
 	 */
-	public void setHandlerCommand(HandlerCommand handlerCommand) {
-		this.handlerCommand = handlerCommand;
+	public void setAgent(String agent) {
+		this.agent = agent;
 	}
 
+	
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Informe um artista");
 		
-		String artista = sc.nextLine();
+		List<String>listArtist = null;
+		listArtist = new ArrayList<String>();
 		
-		try {
-			DataUDP client = new DataUDP(artista, 1090);
-			System.out.println("DataUDP inicializado.");
-			client.listenIt();
-		} finally {
+		listArtist.add("Adele");
+		listArtist.add("Coldplay");
+		listArtist.add("Avril Lavigne");
+		listArtist.add("Bruno Mars");
+		listArtist.add("Rihanna");
+		listArtist.add("Pitty");
+		listArtist.add("Nirvana");
 
-		}
+		
+		DataUDP dataProvider = new DataUDP(5051,"Data provider",listArtist);
+		dataProvider.listenIt();
+		
 	}
+	
 }
