4f1505572cf9d0d2864e92bfe9d4f42ca642af53
olivb@ig.com.br <olivb@ig.com.br@a853e0ac-00ae-8b66-f1aa-5a6682c92272>
2012-03-19 22:15:40 +0000

    Adicionei mais campos no request para facilitar as respostas

--- discografia/src/rn/ufrn/dimap/Request.java
+++ discografia/src/rn/ufrn/dimap/Request.java
@@ -9,15 +9,21 @@ package rn.ufrn.dimap;
 public final class Request {
 	
 	private Commands command;
-	private String argument;
+	private String argument=null;
+	private String host = null;
+	private int port;
+	private String type;
 	
 	/**
 	 * Controi o objeto requiscao
 	 * @param command enum de comandos da aplicacao
 	 */
-	public Request(Commands command,String argument) {
+	public Request(Commands command,String argument,String type, String host,int port) {
 		this.command=command;
 		this.argument=argument;
+		this.host = host;
+		this.port = port;
+		this.type = type;
 	}
 	
 	/**
@@ -46,6 +52,48 @@ public final class Request {
 	 */
 	public void setArgument(String argument) {
 		this.argument = argument;
+	}
+
+	/**
+	 * @return the host
+	 */
+	public String getHost() {
+		return host;
+	}
+
+	/**
+	 * @param host the host to set
+	 */
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	/**
+	 * @return the port
+	 */
+	public int getPort() {
+		return port;
+	}
+
+	/**
+	 * @param port the port to set
+	 */
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
 	}
 
 	
