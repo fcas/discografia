545c9a14e9b2e764fc336069204e5a1041ba2257
olivb@ig.com.br <olivb@ig.com.br@a853e0ac-00ae-8b66-f1aa-5a6682c92272>
2012-03-19 10:22:23 +0000

    Classe finalizada

--- trunk/discografia/src/rn/ufrn/dimap/UDPSendMessage.java
+++ trunk/discografia/src/rn/ufrn/dimap/UDPSendMessage.java
@@ -4,75 +4,77 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.SocketException;
+import java.net.UnknownHostException;
 
-
-/**
- * Classe para enviar mensagens UDP
- */
 public final class UDPSendMessage {
-	
+
+	private UDPMessageContent messageContent = null;	
 	private DatagramSocket socket = null;
 	private DatagramPacket packet = null;
-	private InetAddress serverAdress = null;
-	private UDPMessageContent udpMessageContent = null;	
+	private byte[] buffer = null;
+	private InetAddress inetAdress = null;
 	private int port;
-	
-	
-	/**
-	 * Constroi o objeto de para envio da mensagem UDP
-	 * @param content o corpa da mensagem
-	 * @param serverAdress o endereco de destino
-	 * @param port a porta de destino
-	 * @throws SocketException repassando o erro para componente 
-	 */
-	public UDPSendMessage(String content, InetAddress serverAdress, int port) throws SocketException {
-		this.socket = new DatagramSocket();
-		this.serverAdress = serverAdress;
-		this.port = port;
-		setContent(content);		
-	}
-	
-	
-	/**
-	 * Envia a mensagem UDP
-	 * @throws IOException
-	 */
-	public void sender() throws IOException{
+
+	public UDPSendMessage(final byte[] buffer) {
 		
-		packet = new DatagramPacket(udpMessageContent.getArrayByte(), 
-				udpMessageContent.getArrayByte().length, serverAdress, port);
+		this.messageContent = new UDPMessageContent(buffer);
+		setBuffer(messageContent.getBytes());
+		
+	}
+	
+	/**
+	 * @param buffer the buffer to set
+	 */
+	public void setBuffer(byte[] buffer) {
+		this.buffer = buffer;
+	}
+	
+	/**
+	 * @return the packet
+	 */
+	public DatagramPacket getPacket() {
+		return packet;
+	}
+	
+	/**
+	 * @return the socket
+	 */
+	public DatagramSocket getSocket() {
+		return socket;
+	}
+	
+
+	/**
+	 * @param socket the socket to set
+	 * @throws UnknownHostException 
+	 */
+	public void setSocket(final DatagramSocket socket) throws UnknownHostException {
+		this.socket = socket;
+	}
+
+	/**
+	 * @param port the port to set
+	 */
+	public void setPort(final int port) {
+		this.port = port;
+	}
+
+	/**
+	 * @param inetAdress the inetAdress to set
+	 * @throws UnknownHostException 
+	 */
+	public void setInetAdress(final String host) throws UnknownHostException {
+		this.inetAdress = InetAddress.getByName(host);
+	}
+	
+	public void send() throws IOException{
+		
+		socket = new DatagramSocket();
+		socket.connect(inetAdress,port);
+			
+		packet = new DatagramPacket(buffer,buffer.length);
 		socket.send(packet);
-		closeSocket();		
-	}
-	
-	/**
-	 * Configura o conteudo a mensagem
-	 * @param text
-	 */
-	private void setContent(String text){
-		udpMessageContent = new UDPMessageContent();
-		udpMessageContent.setText(text);
-	}
-	
-	/**
-	 * Fecha o sokete utilizado
-	 */
-	private void closeSocket(){
-		if (socket !=null)
-			socket.close();
-	}
-	
-	// teste simple de envio de mensagem
-	public static void main(String[] args) {
-		try {
-			
-			System.out.println("cliente enviando ");
-			new UDPSendMessage("ECHO:ECHO", InetAddress.getByName("localhost"),1025).sender();
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		
 	}
 	
 }
